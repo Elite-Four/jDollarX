9772c86c5007ea12ed06154d240618833b7c5892
George Chung <Gerhut@GMail.com>
2016-01-29 11:41:32 +0800

    add unit test

--- test.js
+++ test.js
@@ -3,9 +3,14 @@ var cheerio = require('cheerio')
 var proxyquire = require('proxyquire')
 var J$X = proxyquire('./jDollarX', { 'jquery': cheerio })
 
-describe('Tag name', function () {
-  it('should build with string name', function () {
+describe('Build with tag name', function () {
+  it('should works', function () {
     var $el = <div></div>
     $el.is('div').should.ok
+  })
+
+  it('should works with nested elements', function () {
+    var $el = <div><span></span></div>
+    $el.children('span').size().should.be.equal(1)
   })
 })
